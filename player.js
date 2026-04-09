506dd52eb3e4f75e299beb4be89e2fca8c070eaf
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2026-04-09 13:25:45 -0400

    Add documentation for humans, agents, and script loading utility

--- player.js
+++ player.js
@@ -1,3 +1,13 @@
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement("script");
+        script.src = src;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+    });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     // region Configuration Defaults
 
