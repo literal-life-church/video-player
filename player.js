649b877bb85560778cde41ec449cfcfe1ef81539
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2026-04-09 13:52:37 -0400

    Add offline mode support

--- player.js
+++ player.js
@@ -13,7 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const CONTAINER_ID = "literal-life-church-video-player";
     const DEFAULT_ASPECT_RATIO = "16 / 9";
+    const DEFAULT_OFFLINE_MESSAGE = "This event is offline.";
     const LOGGING_TAG = "[Literal Life Church Video Player]";
+    const MARKED_URL = "https://cdn.jsdelivr.net/npm/marked@18.0.0/lib/marked.umd.min.js";
 
     // endregion
 
@@ -42,10 +44,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const aspectRatio = container.dataset.aspectRatio || DEFAULT_ASPECT_RATIO;
     container.style.aspectRatio = aspectRatio;
 
+    const offlineMessage = container.dataset.offlineMessage?.trim() || DEFAULT_OFFLINE_MESSAGE;
+
     // endregion
 
     fetch(apiEndpoint)
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then(async (data) => {
+            if (data.status === "offline") {
+                await loadScript(MARKED_URL);
+                container.innerHTML = `<div>${marked.parse(offlineMessage)}</div>`;
+            }
+        })
         .catch((error) => console.error(error));
 });
